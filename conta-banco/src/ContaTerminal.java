64dad946d7c7b75a94cec04de90c9ef9762d1b38
Carla Muller <carla.ad.muller@gmail.com>
2023-09-09 13:19:01 -0300

    inicial

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Por favor, informe seu nome completo.");
+		String cliente = entrada.next();
+		
+		System.out.println("Por favor, informe o número da conta.");
+		int conta = entrada.nextInt();
+		
+		System.out.println("Por favor, informe o número da agência.");
+		String agencia = entrada.next();
+		
+		System.out.println("Por favor, informe o saldo.");
+		double saldo = entrada.nextDouble();
+		
+		System.out.println("Olá " + cliente + ", obrigado por criar uma conta em nosso banco. Sua agencia é " + agencia + ", conta " + conta + ", e seu saldo R$" + saldo + " já está disponível para saque.");
+		
+		
+		entrada.close();
+		
+	}
+}
